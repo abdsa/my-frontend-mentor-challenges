3395e15396f458c2e0c36fbf767aa9919005d849
abdsa <abd.sab111@gmail.com>
2022-10-19 15:35:32 +0300

    partially done

--- pages/chat-app-css-illustration.tsx
+++ pages/chat-app-css-illustration.tsx
@@ -2,6 +2,10 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import avatar from "../public/c-a-c-i-images/avatar.jpg";
+import dogImage1 from "../public/c-a-c-i-images/dog-image-1.jpg";
+import dogImage2 from "../public/c-a-c-i-images/dog-image-2.jpg";
+import dogImage3 from "../public/c-a-c-i-images/dog-image-3.jpg";
+
 import styles from "../styles/c-a-c-i.module.css";
 Image;
 const ChatAppIllustration: NextPage = () => {
@@ -11,7 +15,7 @@ const ChatAppIllustration: NextPage = () => {
         <title>Dowlk</title>
       </Head>
       <main className="grid grid-cols-1 grid-rows-2 place-items-center lg:grid-cols-2 lg:grid-rows-1">
-        <div className="relative grid  h-[406px] w-[200px] rounded-[20px] p-[8px] shadow-[9px_20px_40px_1px_rgb(0,0,0,0.1)]">
+        <div className="relative grid h-[450px] w-[200px] rounded-[20px] p-[8px] shadow-[9px_20px_40px_1px_rgb(0,0,0,0.1)]">
           <div className="notch absolute left-[28%] h-6 w-24 rounded-b-xl bg-white"></div>
           <div className="h-full w-full rounded-[15px] bg-light-grayish-violet-app-background">
             <div className="contact h-14 w-full rounded-t-[15px] rounded-b-[5px] bg-gradient-to-r from-light-violet to-light-magenta px-2 pt-5 text-white shadow-[0px_10px_10px_0px_rgb(0,0,0,0.1)]">
@@ -33,15 +37,39 @@ const ChatAppIllustration: NextPage = () => {
                 </div>
               </div>
             </div>
-            <div className="grid gap-2 px-[7px] py-3">
-              <div className="text-moderate-violet-chat-on-the-left text-[8px] rounded-[10px] rounded-bl-[5px] p-2 bg-[#ECE5F3] max-w-[130px] break-words">
+            <div className="grid gap-2 px-[7px] py-3 text-[7.5px]">
+              <div className="max-w-[130px] break-words rounded-[10px] rounded-bl-[5px] bg-[#ECE5F3] p-2 text-moderate-violet-chat-on-the-left">
                 That sounds great. I&apos;d be happy with that.
               </div>
-              <div className="text-moderate-violet-chat-on-the-left text-[8px] rounded-[10px] rounded-bl-[5px] p-2 bg-[#ECE5F3] max-w-[130px] break-words">
+              <div className="max-w-[130px] break-words rounded-[10px] rounded-bl-[5px] bg-[#ECE5F3] p-2 text-moderate-violet-chat-on-the-left">
                 Could you send over some pictures of your dog, please?
               </div>
-              <div className="place-self-end text-[8px] rounded-[10px] rounded-bl-[5px] p-2 bg-white max-w-[130px] break-words shadow-lg">
+              <div className="place-self-end grid grid-rows-1 grid-cols-3 gap-2 max-w-[130px]">
+                <Image
+                  src={dogImage1}
+                  alt="a happy dog"
+                  className="rounded-[10px]"
+                />
+                <Image
+                  src={dogImage2}
+                  alt="a happy dog"
+                  className="rounded-[10px]"
+                />
+                <Image
+                  src={dogImage3}
+                  alt="a happy dog"
+                  className="rounded-[10px]"
+                />
+              </div>
+
+              <div className="max-w-[130px] place-self-end break-words rounded-[10px] rounded-br-[5px] bg-white p-2 shadow-lg">
                 Here are a few pictures. She&apos;s a happy girl!
+              </div>
+              <div className="max-w-[130px] place-self-end break-words rounded-[10px] rounded-br-[5px] bg-white p-2 shadow-lg">
+                Can you make it?
+              </div>
+              <div className="max-w-[130px] break-words rounded-[10px] rounded-bl-[5px] bg-[#ECE5F3] p-2 text-moderate-violet-chat-on-the-left">
+                She looks so happy! The time we discussed works. How long shall I take her out for?
               </div>
             </div>
           </div>
